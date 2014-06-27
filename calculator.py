e4d9c0b0fb36c8b314356e18616feb7eeaad44c3
Felix Zeygeril <ironfelya@icloud.com>
2014-06-27 16:18:51 +0000

    logic update

--- calculator.py
+++ calculator.py
@@ -1,14 +1,21 @@
-import math
 import sys
 
 print("Welcome to my first calculator v1.0")
 
+
 def cont():
     ch = raw_input("Press x to exit, c to continue: ")
     if ch.lower() == 'c':
-	pass
+        pass
     else:
-	sys.exit()
+        sys.exit()
+
+
+def read_input():
+    x = input("First number: ")
+    y = input("Second number: ")
+
+    return [x, y]
 
 
 def calculator():
@@ -16,32 +23,29 @@ def calculator():
     print("  B)Subtraction")
     print("  C)Multiplication")
     print("  D)Division")
+    x, y = read_input()
     choice = raw_input("Please pick a letter: ")
     if choice == "A" or choice == "a":
         print("Addition")
-        x = input("First Number: ")
-        y = input("Second Number: ")
         print("Your answer is: " + str(x + y))
         #raw_input("Press <Enter> to exit")
-	cont()
+        cont()
     elif choice == "B" or choice == "b":
         print("Subtraction")
-        x = input("First Number: ")
-        y = input("Second Number: ")
         print("Your answer is: " + str(x - y))
-	cont()
+        cont()
     elif choice == "C" or choice == "c":
         print("Multiplication")
-        x = input("First Number: ")
-        y = input("Second Number: ")
         print("Your answer is: " + str(x * y))
-	cont()
+        cont()
     elif choice == "D" or choice == "d":
         print("Division")
-        x = input("First Number: ")
-        y = input("Second Number: ")
-        print("Your answer is: " + str(x / y))
-	cont()
+        try:
+            ans = x / y
+            print("Your answer is: " + str(ans))
+        except:
+            print 'You can\'t divide by 0 or do other stupid things'
+        cont()
     else:
         print("Please make a valid choice. Please try again")
 
